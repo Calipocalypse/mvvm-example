faa4c2bda44a7b935664d58bd806d28129800b69
Calipocalypse <calipocalypse@gmail.com>
2022-05-28 19:39:30 +0200

    Basic binding

---
+++ ViewModels/ViewAViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvm_example.ViewModels
+{
+    internal class ViewAViewModel : BindableBase
+    {
+        private string firstName = "Test";
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { SetProperty(ref firstName, value); }
+        }
+    }
+}
