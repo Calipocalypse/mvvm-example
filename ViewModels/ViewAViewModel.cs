d43ef8bdaaef20a696abb0dbeefd0d84d8115578
Calipocalypse <calipocalypse@gmail.com>
2022-05-29 11:15:24 +0200

    Delegate Command example + Validation of data

--- ViewModels/ViewAViewModel.cs
+++ ViewModels/ViewAViewModel.cs
@@ -1,14 +1,35 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace mvvm_example.ViewModels
 {
     internal class ViewAViewModel : BindableBase
     {
+        /* Commands */
+        public DelegateCommand ClickMeCommand { get; set; }
+
+        public ViewAViewModel()
+        {
+            ClickMeCommand = new DelegateCommand(Execute, CanExecute).ObservesProperty(() => FirstName);
+        }
+
+        private bool CanExecute()
+        {
+            return !String.IsNullOrEmpty(FirstName);
+        }
+
+        private void Execute()
+        {
+            MessageBox.Show("Clicked!");
+        }
+
         private string firstName = "Test";
 
         public string FirstName
@@ -17,4 +38,5 @@ namespace mvvm_example.ViewModels
             set { SetProperty(ref firstName, value); }
         }
     }
+
 }
